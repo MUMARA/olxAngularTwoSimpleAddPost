0e5c716f80d93bab24ccdcdc613a2b3351cca051
Umar <muhammadumar02@gmail.com>
2016-11-09 08:24:08 +0500

    again error in pending

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   template: `
     <h1>Angular Router</h1>
     <nav>
-      <a routerLink="AddSubmitComponent" routerLinkActive="active">
-      <input type="file" />
+      <a routerLink="AddSubmitComponent" routerLinkActive="active">Add submit page
+     
       </a>
     </nav>
     <router-outlet></router-outlet>
--- app/submitAddPage.ts
+++ app/submitAddPage.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   template: `
-    <h2>CRISIS CENTER</h2>
-    <p>Get your crisis here</p>`
+    <h2>Your Add Submit Page</h2>
+   
+ <input type="file" />`
 })
 export class AddSubmitComponent { }
